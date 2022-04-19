e3ebb003b0cd15454c20ba1775c429f9462969ad
harryzhang1018 <79550723+harryzhang1018@users.noreply.github.com>
2022-04-19 12:02:32 -0500

    Add files via upload

---
+++ track/track_austin.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import size
+from scipy import interpolate
+
+ 
+#track_data = pd.read_csv(r'C:\Users\lenovo\Documents\ME468\FinalProject\racetrack-database\tracks\Austin.csv')
+#print(track_data)
+file = open("Austin.csv")
+data = np.loadtxt(file,delimiter=",")
+#print(data)
+plt.plot(data[:,0],data[:,1])
+plt.show()
